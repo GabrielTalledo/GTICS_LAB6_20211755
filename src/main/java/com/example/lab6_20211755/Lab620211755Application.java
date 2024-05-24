987963d77b920f116ae7430a857c6ead2614a961
GabrielTalledo <gabriel_alejandro.tm@hotmail.com>
2024-05-23 19:14:45 -0500

    feat: start of lab

---
+++ src/main/java/com/example/lab6_20211755/Lab620211755Application.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20211755;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab620211755Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab620211755Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab6_20211755/Lab620211755ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20211755;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620211755ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
